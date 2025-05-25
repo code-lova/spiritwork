4ebfdbac20eca2b52dd7d8849d0106cc601880f9
code-lova <ebinuga@hotmail.com>
2025-05-25 23:29:52 +0100

    doc: fix auth route conflict

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::post('/forgot-password', function (Request $request) {
     return $status === Password::RESET_LINK_SENT
                 ? back()->with(['status' => __($status)])
                 : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
+})->middleware('guest')->name('custom.password.email');
 
 //Updating the Users password
 Route::post('/reset-password', function (Request $request) {
